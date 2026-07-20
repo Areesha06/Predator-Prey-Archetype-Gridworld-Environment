9cc4883d14ded4688ab6ec8a331c1a82c20fb316
Areesha06 <areeshakashif287@gmail.com>
2026-07-21 00:08:04 +0500

    feat: add A2C algorithm implementation with actor-critic networks and training script

--- src/baselines/__init__.py
+++ src/baselines/__init__.py
@@ -3,3 +3,4 @@ from baselines.IQL.iql import IQL  # noqa: F401
 from baselines.CQL.cql import CQL  # noqa: F401
 from baselines.MIXED.mix_train import MixedTrainer  # noqa: F401
 from baselines.DQN.dqn import DQN  # noqa: F401
+from baselines.A2C.a2c import A2C
